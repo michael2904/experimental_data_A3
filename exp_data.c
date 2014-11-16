cd8cd6de18a9a439202052f2fdaf0b83da590c01
Michael ABDALLAH-MINCIOTTI <michael.abdallah-minciotti@mail.mcgill.ca>
2014-11-15 23:25:21 -0500

    comment and used makefile

--- exp_data.c
+++ exp_data.c
@@ -1,39 +1,44 @@
-//
-//  main.c
-//  experimental_data
-//
-//  Created by Michael Abdallah-Minciotti on 2014-11-11.
-//  Copyright (c) 2014 Michael Abdallah-Minciotti. All rights reserved.
-//
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
 int main(int argc, const char * argv[]) {
+    //create the new file to open
+
         FILE * fp;
         char *eNum=NULL;
+    //open the file
         fp = fopen (argv[1], "r");
+    //check if null
         if(fp!= NULL){
+            //create var for line
             char sentence[4096];
             int i=0;
+            //get everything in file
             while (fgets(sentence,sizeof(sentence),fp)){
+                //initialize values
                 double sum=0;
                 i++;
+                //tokenise the line with every ","
                 eNum = strtok(sentence, ",");
                 int j=-1;
+                //do while to get every value
                 while (eNum!=NULL) {
                     j++;
                     if(j==0){
+                        //check if its the name
                         printf("%s :",eNum);
                     }
                     else{
+                        //check if its value
                         sum+=atof(eNum);
                     }
+                    //continue token
                     eNum = strtok(NULL,",");
                 
                     }
+                //do and print each average
                 float average=sum/j;
                 printf("average: %.2f \n",average);
 
@@ -41,7 +46,9 @@ int main(int argc, const char * argv[]) {
 
             }
         else{printf("no such file");}
-        fclose(fp);
+    //finish by closing
+    fclose(fp);
 
     return 0;
 }
+
